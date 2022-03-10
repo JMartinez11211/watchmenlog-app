d0022d25890334d9e618229aa0680b72c94f53cb
Jesus Martinez <jesusmartinez596@gmail.com>
2022-03-09 19:30:46 -0500

    cancel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AddReport from "./components/AddReport";
@@ -46,7 +23,6 @@ function App() {
         
       </div>
     </BrowserRouter>
->>>>>>> parent of 3e0877c (Updates)
   );
 }
 
